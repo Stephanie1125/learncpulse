a988835a6acd5fbbec9bb794357084f532fd6246
stephanie <stephanieliu1125@gmail.com>
2016-07-23 16:26:47 +0900

    add hello

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+//
+//  hello.cpp
+//  learncpulse
+//
+//  Created by stephanie on 2016/7/23.
+//  Copyright © 2016年 stephanie. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cout << "Hello, World!\n";
+    cout << "Hi, c++!\n";
+    return 0;
+}
